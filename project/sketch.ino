d79f11ecc4148d71dfb979bf2c419abb9fdb03d3
DecodeLabs-Internshipfemina <femina9617@gmail.com>
2026-06-30 02:33:05 +0530

    week 3 iot project

--- project/sketch.ino
+++ project/sketch.ino
@@ -1,26 +1,63 @@
-const int sensorPin = 34;
-const int relayPin = 2;
+#include <WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 
-int threshold = 2000;
+#define WLAN_SSID       "OnePlus Nord CE 2 Lite 5G"
+#define WLAN_PASS       "shamoosa"
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+
+#define AIO_USERNAME    "feminacm"
+#define AIO_KEY         "aio_otgl66foZEzg2Z0eq1fWk0AGOWXK"
+
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT,
+                          AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish distanceFeed =
+  Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/distance");
+
+#define TRIG_PIN 5
+#define ECHO_PIN 18
 
 void setup() {
   Serial.begin(115200);
-  pinMode(relayPin, OUTPUT);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi Connected");
 }
 
 void loop() {
-  int moisture = analogRead(sensorPin);
-
-  Serial.print("Moisture: ");
-  Serial.println(moisture);
-
-  if (moisture < threshold) {
-    digitalWrite(relayPin, HIGH);
-    Serial.println("Pump ON");
-  } else {
-    digitalWrite(relayPin, LOW);
-    Serial.println("Pump OFF");
+  if (mqtt.connect() != 0) {
+    mqtt.disconnect();
+    delay(5000);
+    return;
   }
 
-  delay(1000);
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.0343 / 2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  distanceFeed.publish(distance);
+
+  delay(2000);
 }
